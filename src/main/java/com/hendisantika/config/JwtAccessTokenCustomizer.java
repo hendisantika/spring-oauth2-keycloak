11864b9790424a7c6336e7df81437394196ddd44
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:35:34 +0700

    #7 - Add extractRoles in JwtAccessTokenCustomizer :whale: :bowtie:

--- src/main/java/com/hendisantika/config/JwtAccessTokenCustomizer.java
+++ src/main/java/com/hendisantika/config/JwtAccessTokenCustomizer.java
@@ -8,11 +8,13 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccess
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -72,5 +74,20 @@ public class JwtAccessTokenCustomizer extends DefaultAccessTokenConverter implem
         Authentication usernamePasswordAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), "N/A", authorities);
         LOG.debug("End extractAuthentication");
         return new OAuth2Authentication(request, usernamePasswordAuthentication);
+    }
+
+    private List<GrantedAuthority> extractRoles(JsonNode jwt) {
+        LOG.debug("Begin extractRoles: jwt = {}", jwt);
+        Set<String> rolesWithPrefix = new HashSet<>();
+
+        jwt.path(CLIENT_NAME_ELEMENT_IN_JWT)
+                .elements()
+                .forEachRemaining(e -> e.path(ROLE_ELEMENT_IN_JWT)
+                        .elements()
+                        .forEachRemaining(r -> rolesWithPrefix.add("ROLE_" + r.asText())));
+
+        final List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList(rolesWithPrefix.toArray(new String[0]));
+        LOG.debug("End extractRoles: roles = {}", authorityList);
+        return authorityList;
     }
 }
