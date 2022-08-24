7fb4c68cbb88442ce78453bb8ad81a7fc3e028eb
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:43:45 +0700

    #24 - Add getAuthorizedUserName in EmployeeRestController :whale: :bowtie:

--- src/main/java/com/hendisantika/controller/EmployeeRestController.java
+++ src/main/java/com/hendisantika/controller/EmployeeRestController.java
@@ -1,5 +1,9 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.config.SecurityContextUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeRestController {
+    @GetMapping(path = "/username")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    public ResponseEntity<String> getAuthorizedUserName() {
+        return ResponseEntity.ok(SecurityContextUtils.getUserName());
+    }
 }
