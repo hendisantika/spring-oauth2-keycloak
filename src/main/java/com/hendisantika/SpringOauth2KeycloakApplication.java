b16222386fe170d200c6a73475bfd6c5d3b0f182
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:33:18 +0700

    #3 - Add Keycloak Configs :whale: :bowtie:

--- src/main/java/com/hendisantika/SpringOauth2KeycloakApplication.java
+++ src/main/java/com/hendisantika/SpringOauth2KeycloakApplication.java
@@ -2,8 +2,11 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class})
 public class SpringOauth2KeycloakApplication {
 
     public static void main(String[] args) {
