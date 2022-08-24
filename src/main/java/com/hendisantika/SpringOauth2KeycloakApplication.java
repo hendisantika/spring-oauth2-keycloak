f2319bb81b2f44d923083f53f12e338dfb48e720
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:29:48 +0700

    #1 - Initial Commit :whale: :bowtie:

---
+++ src/main/java/com/hendisantika/SpringOauth2KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2KeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2KeycloakApplication.class, args);
+    }
+
+}
