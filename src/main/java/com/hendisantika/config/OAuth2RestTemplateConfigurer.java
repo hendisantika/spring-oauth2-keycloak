b6dfa8ba668499bd215263dc958b05caca8d25cf
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:37:26 +0700

    #10 - Add oauth2RestTemplate in OAuth2RestTemplateConfigurer :whale: :bowtie:

--- src/main/java/com/hendisantika/config/OAuth2RestTemplateConfigurer.java
+++ src/main/java/com/hendisantika/config/OAuth2RestTemplateConfigurer.java
@@ -2,8 +2,11 @@ package com.hendisantika.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +23,15 @@ import org.springframework.context.annotation.Configuration;
 public class OAuth2RestTemplateConfigurer {
 
     private static final Logger LOG = LoggerFactory.getLogger(OAuth2RestTemplateConfigurer.class);
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails details) {
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details);
+
+        LOG.debug("Begin OAuth2RestTemplate: getAccessToken");
+        /* To validate if required configurations are in place during startup */
+        oAuth2RestTemplate.getAccessToken();
+        LOG.debug("End OAuth2RestTemplate: getAccessToken");
+        return oAuth2RestTemplate;
+    }
 }
