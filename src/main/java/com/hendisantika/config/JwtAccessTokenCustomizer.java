f0d613d204828bddb2f8754fba16b4f3250ddd98
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:35:16 +0700

    #6 - Add extractAuthentication in JwtAccessTokenCustomizer :whale: :bowtie:

--- src/main/java/com/hendisantika/config/JwtAccessTokenCustomizer.java
+++ src/main/java/com/hendisantika/config/JwtAccessTokenCustomizer.java
@@ -1,11 +1,21 @@
 package com.hendisantika.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterConfigurer;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +47,30 @@ public class JwtAccessTokenCustomizer extends DefaultAccessTokenConverter implem
     public void configure(JwtAccessTokenConverter converter) {
         converter.setAccessTokenConverter(this);
         LOG.info("Configured {}", JwtAccessTokenConverter.class.getSimpleName());
+    }
+
+    /**
+     * Spring oauth2 expects roles under authorities element in tokenMap, but keycloak provides it under resource_access. Hence extractAuthentication
+     * method is overriden to extract roles from resource_access.
+     *
+     * @return OAuth2Authentication with authorities for given application
+     */
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> tokenMap) {
+        LOG.debug("Begin extractAuthentication: tokenMap = {}", tokenMap);
+        JsonNode token = mapper.convertValue(tokenMap, JsonNode.class);
+        Set<String> audienceList = extractClients(token); // extracting client names
+        List<GrantedAuthority> authorities = extractRoles(token); // extracting client roles
+
+        OAuth2Authentication authentication = super.extractAuthentication(tokenMap);
+        OAuth2Request oAuth2Request = authentication.getOAuth2Request();
+
+        OAuth2Request request =
+                new OAuth2Request(oAuth2Request.getRequestParameters(), oAuth2Request.getClientId(), authorities, true, oAuth2Request.getScope(),
+                        audienceList, null, null, null);
+
+        Authentication usernamePasswordAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), "N/A", authorities);
+        LOG.debug("End extractAuthentication");
+        return new OAuth2Authentication(request, usernamePasswordAuthentication);
     }
 }
