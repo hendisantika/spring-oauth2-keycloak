2581a28791b007bf1220ce3c6199d8b6e142bfa3
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 21:29:04 +0700

    #34 - Add KeycloakSecurityConfiguration :pencil2: :green_book: :seven:

---
+++ src/main/java/com/hendisantika/config/KeycloakSecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.hendisantika.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/24/22
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+@KeycloakConfiguration
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true", matchIfMissing = true)
+public class KeycloakSecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http.csrf()
+                .disable()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
