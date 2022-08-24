1db38440ee6864b0d20e2f950770936a244162b0
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:43:27 +0700

    #23 - Add EmployeeRestController :whale: :bowtie:

---
+++ src/main/java/com/hendisantika/controller/EmployeeRestController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/24/22
+ * Time: 18:43
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeRestController {
+}
