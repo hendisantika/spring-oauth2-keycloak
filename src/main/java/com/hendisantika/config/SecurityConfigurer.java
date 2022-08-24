90ea3bb2b875d8d797e40524428f57853a8f604d
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:38:27 +0700

    #12 - Add SecurityConfigurer :whale: :bowtie:

---
+++ src/main/java/com/hendisantika/config/SecurityConfigurer.java
@@ -0,0 +1,40 @@
+package com.hendisantika.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/24/22
+ * Time: 18:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ConditionalOnProperty(prefix = "rest.security", value = "enabled", havingValue = "true")
+@Import({SecurityProperties.class})
+public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
+
+    private final ResourceServerProperties resourceServerProperties;
+
+    private final SecurityProperties securityProperties;
+
+    /* Using spring constructor injection, @Autowired is implicit */
+    public SecurityConfigurer(ResourceServerProperties resourceServerProperties, SecurityProperties securityProperties) {
+        this.resourceServerProperties = resourceServerProperties;
+        this.securityProperties = securityProperties;
+    }
+}
