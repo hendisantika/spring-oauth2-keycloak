62192829d18fbaf5bb971f82cb31f4927010420a
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-24 18:39:06 +0700

    #14 - Add configure in SecurityConfigurer :whale: :bowtie:

--- src/main/java/com/hendisantika/config/SecurityConfigurer.java
+++ src/main/java/com/hendisantika/config/SecurityConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceS
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -42,5 +43,23 @@ public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId(resourceServerProperties.getResourceId());
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+
+        http.cors()
+                .configurationSource(corsConfigurationSource())
+                .and()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(securityProperties.getApiMatcher())
+                .authenticated();
+
     }
 }
